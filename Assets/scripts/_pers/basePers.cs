90b6699fcf7f7d227c3f67146610a6b7561648dc
Vladislav Kolesnichenko <45514253+Vladislav123koleso@users.noreply.github.com>
2024-09-19 11:05:56 +0300

    blocking part 1
    создание блокинга главной сцены

---
+++ Assets/scripts/_pers/basePers.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+[Serializable]
+public class basePers
+{
+    public int hp;
+    public int mp;
+    public int speed;
+    public int strength;
+}
